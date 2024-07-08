86454e3d5d705ecde612c046fe05a65cdb51d5c8
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-07-08 20:24:37 +0100

    Modified product header styles

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 // mui imports
 import { styled, useTheme } from '@mui/material/styles';
-import { Box, Container, Grid, Typography } from '@mui/material';
+import { Box, Container, Grid, Stack, Typography } from '@mui/material';
 
 // Components
 import ProductCard from '../components/ProductCard';
 
 // Header-Image
 import Hero from '../assets/img/Rectangle 2.png';
+import HeroII from '../assets/img/shop-hero.png';
+
 // Product Images
 import lips1 from '../assets/img/lip/lip-1.png';
 import lips2 from '../assets/img/lip/lip-2.png';
@@ -170,10 +172,11 @@ const Product = () => {
   const Header = styled(Box)(({ theme }) => ({
     position: 'relative',
     width: '100%',
-    height: '80vh',
+    height: '90vh',
     backgroundColor: theme.palette.primary.light,
-    backgroundImage: `url(${Hero})`,
+    backgroundImage: `url(${HeroII})`,
     backgroundSize: 'cover',
+    backgroundPosition: 'left center',
     backgroundRepeat: 'no-repeat',
     overflow: 'hidden',
   }));
@@ -189,24 +192,62 @@ const Product = () => {
 
   const ContentBox = styled(Box)(({ theme }) => ({
     position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    minHeight: '100%', 
     zIndex: 1,
-    color: '#fff',
+    color: theme.palette.secondary.dark,
     padding: theme.spacing(4),
   }));
+
+  const HeaderSX ={
+    fontSize: '3.5rem',
+    fontWeight: '500',
+    maxWidth: '700px'
+  }
+
+  const logo={
+    color: '#EC5766'
+  }
 
   return (
     <>
       <Header>
-        <Overlay />
+        
         <ContentBox>
-          <Grid container spacing={2}>
+          {/* <Overlay  /> */}
+          {/* <Grid container spacing={2}>
             <Grid item xs={6}>
-              <Typography variant="h3">Welcome</Typography>
+              
             </Grid>
             <Grid item xs={6}>
               <Typography variant="h3">Home</Typography>
             </Grid>
-          </Grid>
+          </Grid> */}
+          <Stack spacing={2} sx={{marginLeft:{xs:0, lg:5}}} >
+          <Typography variant="h2" sx={HeaderSX}>Start <br />Your Beauty Journey<br /> with <span className='text-[#EC5766]'>Glamazone</span> </Typography>
+          <Typography variant="caption1" sx={{maxWidth: '400px', fontSize:'20px', lineHeight: '30px'}} >
+            Explore our collections, indulge in some self-care, and embrace your inner glam! We can't wait to be a part of your beauty story. 
+          </Typography>
+          {/* Search Bar */}
+          <div className="w-full max-w-lg pt-5" >
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <svg className="w-8 h-8 text-gray-900" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                      d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z">
+                    </path>
+                  </svg>
+                </div>
+
+                <form action="#" method="GET">
+                  <input type="search" name="search" placeholder="Search Products" className="p-4 pl-14 text-lg text-gray-600 rounded-3xl w-full border border-gray-600" />
+                </form>
+              </div>
+            </div>
+            </Stack>
         </ContentBox>
       </Header>
       {/* Product Listing */}
